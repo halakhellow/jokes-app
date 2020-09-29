f2b9653c7b22b6cc70db442999424618d3c07ed2
Hala Alkhellow <halakhellow1996@gmail.com>
2020-09-29 16:31:36 +0300

    fetch  jokes from api after initial rendering & render them

--- src/JokesList.js
+++ src/JokesList.js
@@ -1,8 +1,38 @@
 import React, { Component } from "react";
 
 class JokesList extends Component {
+  static defaultProps = {
+    jokesNum: 10,
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: [],
+    };
+  }
+
+  async componentDidMount() {
+    let jokes = [];
+    //while loop is sync and fetch is async => that's why we have to use async await here
+    while (jokes.length < this.props.jokesNum) {
+      let res = await fetch("https://icanhazdadjoke.com/", {
+        headers: { Accept: "application/json" },
+      });
+      let data = await res.json();
+      jokes.push(data.joke);
+    }
+    this.setState({ jokes: jokes });
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>Dad Jokes</h1>
+        {this.state.jokes.map((joke) => (
+          <div>{joke}</div>
+        ))}
+      </div>
+    );
   }
 }
 
