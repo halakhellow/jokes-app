7bfbc3156eef924d4cf4b10e5f1c4cf2880c6546
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-01 13:59:01 +0300

    Updates

---
+++ precache-manifest.79b6deaa4fa84ce8cae911f249f2f667.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f786fc1ad2e03e2a86373adb7b10c59",
+    "url": "/jokes-app/index.html"
+  },
+  {
+    "revision": "c73f71d1643b5ec470ef",
+    "url": "/jokes-app/static/css/main.7530ccfc.chunk.css"
+  },
+  {
+    "revision": "f2482a7f73ab34c41a42",
+    "url": "/jokes-app/static/js/2.15fed54f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jokes-app/static/js/2.15fed54f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c73f71d1643b5ec470ef",
+    "url": "/jokes-app/static/js/main.900e4340.chunk.js"
+  },
+  {
+    "revision": "ebae275c1d737270c166",
+    "url": "/jokes-app/static/js/runtime-main.48a26c1e.js"
+  },
+  {
+    "revision": "a4b8080a1a1326983f84b0d505e38da7",
+    "url": "/jokes-app/static/media/emoji.a4b8080a.png"
+  }
+]);
