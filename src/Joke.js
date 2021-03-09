fba165b5fbb87dfe267f84892c5257500ffaf2c4
Hala Alkhellow <halakhellow1996@gmail.com>
2021-03-10 01:06:20 +0200

    use different classes to change the emoji when votes change in Joke component

--- src/Joke.js
+++ src/Joke.js
@@ -17,10 +17,10 @@ class Joke extends Component {
     let votes = this.props.votes;
     if (votes >= 15) return "em em-rolling_on_the_floor_laughing";
     else if (votes >= 12) return "em em-laughing";
-    else if (votes >= 9) return "em em-smiley";
-    else if (votes >= 6) return "em em-slightly_smiling_face";
-    else if (votes >= 3) return "em em-neutral_face";
-    else if (votes >= 0) return "em em-confused";
+    else if (votes >= 9) return "em em-grin";
+    else if (votes >= 6) return "em em-smiley";
+    else if (votes >= 3) return "em em-slightly_smiling_face";
+    else if (votes >= 0) return "em em-face_with_rolling_eyes";
     else return "em em-angry";
   }
 
