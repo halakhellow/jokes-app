64ac73b91f62867f10a2e54415ff83035ac4c950
Hala Alkhellow <halakhellow1996@gmail.com>
2021-03-12 22:48:42 +0200

    add Loader css file and assign it to Loader component

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+import "./Loader.css";
+
 let Loader = () => {
   return (
-    <div>
+    <div className="Loader">
       <i className="far fa-8x fa-grin-stars fa-spin"></i>
       <h1>Loading ...</h1>
     </div>
