2cb5a9082ca1e9596558cb1f1328a6b64be7c834
Hala Alkhellow <halakhellow1996@gmail.com>
2021-03-12 22:48:08 +0200

    change the clear btn style in CustomBtn depending on props passed from the parent an+ add hovering style for the component

--- src/components/CustomBtn/CustomBtn.js
+++ src/components/CustomBtn/CustomBtn.js
@@ -2,9 +2,12 @@ import React from "react";
 
 import "./CustomBtn.css";
 
-let CustomBtn = ({ text, handleClick }) => {
+let CustomBtn = ({ text, handleClick, clearBtn }) => {
   return (
-    <button onClick={handleClick} className="CustomBtn">
+    <button
+      onClick={handleClick}
+      className={`CustomBtn ${clearBtn && "clearBtn"}`}
+    >
       {text}
     </button>
   );
